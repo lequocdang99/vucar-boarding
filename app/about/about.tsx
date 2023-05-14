830818d5cb6a360527b269160ea9cd26665a49d7
lequocdang99 <120897470+lequocdang99@users.noreply.github.com>
2023-05-14 14:39:57 +0700

    Adjust main page title

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
+import React from 'react';
 
-export interface AboutPageProps {}
-
-export default function App(props: AboutPageProps) {
+export function About() {
   return <div>About Page</div>;
 }
