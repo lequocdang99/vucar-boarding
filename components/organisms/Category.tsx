f154e08cf45d8dc604a7f44823b2b88cb0d19190
lequocdang99 <120897470+lequocdang99@users.noreply.github.com>
2023-05-19 16:09:17 +0700

    fix errors

--- components/organisms/Category.tsx
+++ components/organisms/Category.tsx
@@ -3,20 +3,10 @@ import Head from '../atoms/Head';
 import All from '../atoms/All';
 import Card from '../molecules/Card';
 import { getNews } from '@/app/page';
-
-interface News {
-  author: string;
-  content: string;
-  date: string;
-  imageUrl: string;
-  readMoreUrl: string;
-  time: string;
-  title: string;
-  url: string;
-}
+import { News } from '@/types/types';
 
 export default async function Category({ category }: { category: string }) {
-  const { cat, data, success } = await getNews(category);
+  const data = await getNews(category);
 
   return (
     <div className='grid grid-cols-1 p-5 gap-5 '>
@@ -30,7 +20,7 @@ export default async function Category({ category }: { category: string }) {
         </div>
       </div>
       <div className='grid md:grid-cols-3 sm:grid-cols-1 gap-10 h-fit'>
-        {data.slice(0, 3).map((item: News) => (
+        {data.data.slice(0, 3).map((item: News) => (
           <Card
             category={category}
             item={item}
